662eed3063ea62849041af9d2aa82baa849ecd62
Jakob Voss <voss@gbv.de>
2013-09-06 11:14:51 +0200

    documentation

--- README.md
+++ README.md
@@ -2,7 +2,10 @@ Catmandu::RDF - Modules for handling RDF data within the [Catmandu](http://libre
 
 This [CPAN distribution](https://metacpan.org/release/Catmandu-RDF) is in an
 early state of development. Feedback and contributions are very welcome at
-<https://github.com/nichtich/Catmandu-RDF>!
+<https://github.com/gbv/Catmandu-RDF>!
 
-[![Build Status](https://travis-ci.org/nichtich/Catmandu-RDF.png)](https://travis-ci.org/nichtich/Catmandu-RDF)
-[![Coverage Status](https://coveralls.io/repos/nichtich/Catmandu-RDF/badge.png?branch=master)](https://coveralls.io/r/nichtich/Catmandu-RDF?branch=master)
+# Status
+
+[![Build Status](https://travis-ci.org/gbv/Catmandu-RDF.png)](https://travis-ci.org/gbv/Catmandu-RDF)
+[![Coverage Status](https://coveralls.io/repos/gbv/Catmandu-RDF/badge.png?branch=devel)](https://coveralls.io/r/gbv/Catmandu-RDF?branch=devel)
+[![Kwalitee Score](http://cpants.cpanauthors.org/dist/Catmandu-RDF.png)
--- lib/Catmandu/RDF.pm
+++ lib/Catmandu/RDF.pm
@@ -1,16 +1,53 @@
 package Catmandu::RDF;
-#ABSTRACT: Modules for handling RDF data within the Catmandu framework
-#VERSION
+# ABSTRACT: Modules for handling RDF data within the Catmandu framework
+# VERSION
 
 =head1 DESCRIPTION
 
 Catmandu::RDF contains modules for handling RDF data within the L<Catmandu>
 framework. This release is in an early state of development. Feedback and
-contributions are very welcome at <https://github.com/nichtich/Catmandu-RDF>!
+contributions are very welcome at L<https://github.com/nichtich/Catmandu-RDF>!
 
-=head1 MODULES
+=head1 AVAILABLE MODULES
+
+=over 4
 
 =item L<Catmandu::Exporter::RDF>
+
+Serialize RDF data (as RDF/XML, RDF/JSON, Turtle, NTriples, RDFa...).
+
+=back
+
+=head1 SUGGESTED MODULES
+
+The following modules have not been implemented yet. Please contribute or
+comment if you miss them!
+
+=over 4
+
+=item C<Catmandu::Importer::RDF>
+
+Import serialized RDF data (RDF/XML, RDF/JSON, Turtle, NTriples, RDFa...).
+
+=item C<Catmandu::Importer::SPARQL>
+
+Import RDF data from a SPARQL endpoint.
+
+=item C<Catmandu::Exporter::SPARUL> or C<Catmandu::Exporter::SPARQL>
+
+Export RDF data with SPARQL/Update.
+
+=item C<Catmandu::Exporter::RDFPatch>
+
+Export RDF with HTTP PATCH.
+
+=back
+
+=encoding utf8
+
+=head1 SEE ALSO
+
+L<Catmandu>, L<RDF::Trine>, L<RDF::NS>
 
 =cut
 
