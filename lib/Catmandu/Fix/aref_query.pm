1890f57051685db8f5a276ee9709a5c97d1ca79f
Jakob Voss <voss@gbv.de>
2014-12-11 16:09:13 +0100

    simplify calling syntax of aref_query fix

--- lib/Catmandu/Fix/aref_query.pm
+++ lib/Catmandu/Fix/aref_query.pm
@@ -3,23 +3,41 @@ package Catmandu::Fix::aref_query;
 use Catmandu::Sane;
 use Moo;
 use RDF::aREF::Query;
+use Catmandu::Fix::Has;
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 has query => (
     is => 'ro',
-    coerce => sub { 
-        RDF::aREF::Query->new( query => $_[0] ) 
-        # TODO: ns
-    }
+    coerce => sub { RDF::aREF::Query->new( query => $_[0] ) } # TODO: ns
 );
-has field => (is => 'ro');
+has field => (
+    is => 'ro',
+);
+has subject => (
+    is => 'ro',
+);
+
+around 'BUILDARGS', sub {
+    my $orig = shift;
+    my $self = shift;
+
+    if (@_ == 3) {
+        $orig->($self, subject => $_[0], query => $_[1], field => $_[2] );
+    } elsif (@_ == 2) {
+        $orig->($self, query => $_[0], field => $_[1] );
+    } else {
+        $orig->($self, @_);
+    }
+};
+
 
 sub fix {
     my ($self, $data) = @_;
 
     my $field = $self->field;
-    my $origin = $data->{_uri} // $data->{_url};
+    my $origin = $self->subject // $data->{_uri} // $data->{_url};
+
     my @values = $self->query->apply( $data, $origin );
 
     if (@values) {
@@ -42,6 +60,12 @@ __END__
 
 =head1 NAME
 
-Catmandu::Fix::aref_query
+Catmandu::Fix::aref_query - copy values of RDF in aREF to a new field
+
+=head1 SYNOPSIS
+
+    aref_query( dc_title => title )
+    aref_query( query => 'dc_title', field => 'title' )
+    aref_query( 'http://example.org/subject', dc_title => title )
 
 =cut
--- t/fix-aref-query.t
+++ t/fix-aref-query.t
@@ -5,10 +5,8 @@ use Catmandu ':all';
 
 use_ok 'Catmandu::Fix::aref_query';
 
-my $fixer = Catmandu::Fix::aref_query->new(
-    field => 'title',
-    query => 'dc_title'
-);
+my $fixer = Catmandu::Fix::aref_query->new( 'dc_title' => 'title' );
+is $fixer->field, 'title';
 
 my $rdf = importer('RDF', file => 't/example.ttl')->first;
 ($rdf->{_uri}) = keys %$rdf;
@@ -20,5 +18,9 @@ is_deeply $rdf, {
     '_uri' => 'http://example.org',
     title => "B\x{c4}R"
 }, 'simple RDF fix';
+
+$rdf = importer('RDF', file => 't/example.ttl')->first;
+Catmandu::Fix::aref_query->new('http://example.org','dc_title','label')->fix($rdf);
+is $rdf->{label}, "B\x{c4}R";
 
 done_testing;
